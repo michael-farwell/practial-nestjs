03a37da9e0cec81843488f075a38b03e98f4e0e4
Michael Farwell <mike@lie-nielsen.com>
2022-02-24 14:26:26 -0500

    complete chapter 14

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,16 @@
 import { Module }             from "@nestjs/common";
 import { TypeOrmModule }      from "@nestjs/typeorm";
 import { AppController }      from "./app.controller";
-import { ProductsController } from "./products.controller";
+import { ProductsController } from "./controllers/products.controller";
+import { Product }            from "./models/product.entity";
+import { ProductsService }    from "./services/products.service";
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(),
+    TypeOrmModule.forFeature([Product]),
   ],
   controllers: [AppController, ProductsController],
+  providers: [ProductsService],
 })
 export class AppModule {}
