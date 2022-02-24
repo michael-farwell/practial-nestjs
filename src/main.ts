5d9a2044288d4fcddb9ceb6bda0d14b17a283609
Michael Farwell <mike@lie-nielsen.com>
2022-02-24 07:06:45 -0500

    complete Chapter 5 and 6

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { NestFactory }            from "@nestjs/core";
 import { NestExpressApplication } from "@nestjs/platform-express";
+import * as hbs                   from "hbs";
+import * as hbsUtils              from "hbs-utils";
 import { join }                   from "path";
 import { AppModule }              from "./app.module";
 
@@ -9,6 +11,10 @@ async function bootstrap () {
   app.useStaticAssets(join(__dirname, "..", "public"));
   app.setBaseViewsDir(join(__dirname, "..", "views"));
   app.setViewEngine("hbs");
+
+  hbs.registerPartials(join(__dirname, "..", "views/layouts"));
+  hbsUtils(hbs)
+    .registerWatchedPartials(join(__dirname, "..", "views/layouts"));
 
   await app.listen(3000);
 }
