76de2f91e3f91cb65691c48b1a5ad5e31916379f
Michael Farwell <mike@lie-nielsen.com>
2022-02-28 07:58:07 -0500

    complete chapter 17

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -1,8 +1,10 @@
-import { Module }          from "@nestjs/common";
-import { AdminController } from "./controllers/admin.controller";
+import { Module }                  from "@nestjs/common";
+import { AdminController }         from "./controllers/admin.controller";
+import { AdminProductsController } from "./controllers/admin.products.controller";
 
 @Module({
-  controllers: [AdminController],
+  controllers: [AdminController, AdminProductsController],
 })
 
-export class AdminModule {}
+export class AdminModule {
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module }             from "@nestjs/common";
+import { Global, Module }     from "@nestjs/common";
 import { TypeOrmModule }      from "@nestjs/typeorm";
 import { AdminModule }        from "./admin/admin.module";
 import { AppController }      from "./app.controller";
@@ -6,6 +6,7 @@ import { ProductsController } from "./controllers/products.controller";
 import { Product }            from "./models/product.entity";
 import { ProductsService }    from "./services/products.service";
 
+@Global()
 @Module({
   imports: [
     TypeOrmModule.forRoot(),
@@ -14,5 +15,6 @@ import { ProductsService }    from "./services/products.service";
   ],
   controllers: [AppController, ProductsController],
   providers: [ProductsService],
+  exports: [ProductsService],
 })
 export class AppModule {}
