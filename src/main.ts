39948709f515e321b898124922403159c2da83f3
Michael Farwell <mike@lie-nielsen.com>
2022-03-07 06:31:59 -0500

    complete chapter 24

--- src/main.ts
+++ src/main.ts
@@ -33,6 +33,14 @@ async function bootstrap () {
     next();
   });
 
+  app.use("/admin", function (req, res, next) {
+    if (req.session.user && req.session.user.role == "admin") {
+      next();
+    } else {
+      res.redirect("/");
+    }
+  });
+
   await app.listen(3000);
 }
 
